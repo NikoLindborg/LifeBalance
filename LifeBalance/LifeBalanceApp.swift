2c0072bcb5caab682ebad11ae5e585da386c72ab
NikoLindborg <nlindborg@saunassa.com>
2021-11-15 13:16:13 +0200

    Initial Commit

---
+++ LifeBalance/LifeBalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeBalanceApp.swift
+//  LifeBalance
+//
+//  Created by Niko Lindborg on 15.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct LifeBalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

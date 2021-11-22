8b41ee42e6f6a361e335eb18c961d6ade01efe36
aleksikosonen <kososenaleksi@gmail.com>
2021-11-22 17:47:11 +0200

    Added bottom navigation to application

--- LifeBalance/LifeBalanceApp.swift
+++ LifeBalance/LifeBalanceApp.swift
@@ -11,7 +11,28 @@ import SwiftUI
 struct LifeBalanceApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            TabView {
+                HomeView()
+                    .tabItem() {
+                        Image(systemName: "heart.fill")
+                        Text("Home")
+                    }
+                DiaryView()
+                    .tabItem() {
+                        Image(systemName: "book.fill")
+                        Text("Diary")
+                    }
+                AddMealView()
+                    .tabItem() {
+                        Image(systemName: "plus.circle.fill")
+                        Text("Add meal")
+                    }
+                SettingsView()
+                    .tabItem() {
+                        Image(systemName: "slider.vertical.3")
+                        Text("Settings")
+                    }
+            }
         }
     }
 }

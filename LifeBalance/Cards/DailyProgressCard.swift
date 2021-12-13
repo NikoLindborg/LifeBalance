a08016700d8df05f7a30b97c52ca5b871475303b
aleksikyto <aleksi.kyto@metropolia.fi>
2021-12-13 12:45:21 +0200

    working

--- LifeBalance/Cards/DailyProgressCard.swift
+++ LifeBalance/Cards/DailyProgressCard.swift
@@ -37,52 +37,6 @@ struct DailyProgressCard: View {
                 
                 HStack(){
                     if(progressSettings.progressValues.count > 2){
-                        if(progressSettings.progressValues.count == 3) {
-                            VStack{
-                                Spacer()
-                                HStack{
-                                    Spacer()
-                                    VStack{
-                                        if(progressSettings.progressValues.indices.contains(0)){
-                                            ProgressBar(progress: $progressSettings.progressValues[0].progress, color: self.$color2)
-                                                .frame(width: 50.0, height: 50.0)
-                                            Text("\($progressSettings.progressValues[0].consumed.wrappedValue, specifier: "%.0f") / \($progressSettings.progressValues[0].target.wrappedValue, specifier: "%.0f") \($progressSettings.progressValues[0].unit.wrappedValue)")
-                                                .foregroundColor(Color.white)
-                                            Text($progressSettings.progressValues[0].description.wrappedValue)
-                                                .foregroundColor(Color.white)
-                                        }
-                                    }
-                                    Spacer()
-                                    VStack{
-                                        if(progressSettings.progressValues.indices.contains(1)){
-                                            ProgressBar(progress: $progressSettings.progressValues[1].progress, color: self.$color2)
-                                                .frame(width: 50.0, height: 50.0)
-                                            Text("\($progressSettings.progressValues[1].consumed.wrappedValue, specifier: "%.0f") / \($progressSettings.progressValues[1].target.wrappedValue, specifier: "%.0f") \($progressSettings.progressValues[1].unit.wrappedValue)")
-                                                .foregroundColor(Color.white)
-                                            Text($progressSettings.progressValues[1].description.wrappedValue)
-                                                .foregroundColor(Color.white)
-                                        }
-                                    }
-                                    Spacer()
-                                }
-                                Spacer()
-                                HStack{
-                                    VStack{
-                                        if(progressSettings.progressValues.indices.contains(2)){
-                                            
-                                            ProgressBar(progress: $progressSettings.progressValues[2].progress, color: self.$color2)
-                                                .frame(width: 50.0, height: 50.0)
-                                            Text("\($progressSettings.progressValues[2].consumed.wrappedValue, specifier: "%.0f") / \($progressSettings.progressValues[2].target.wrappedValue, specifier: "%.0f") \($progressSettings.progressValues[2].unit.wrappedValue)")
-                                                .foregroundColor(Color.white)
-                                            Text($progressSettings.progressValues[2].description.wrappedValue)
-                                                .foregroundColor(Color.white)
-                                        }
-                                    }
-                                }
-                                Spacer()
-                            }.frame(height: 300)
-                        }
-                        if(progressSettings.progressValues.count == 4){
                             LazyHGrid(rows: [GridItem(.flexible()),GridItem(.flexible())], alignment: .center, spacing: 80){
                                 ForEach($progressSettings.progressValues) {item in
                                     VStack{
@@ -95,8 +49,6 @@ struct DailyProgressCard: View {
                                     }
                                 }
                             }.frame(height: 300)
-                            
-                        }
                     }
                     else {
                         LazyHGrid(rows: [GridItem(.fixed(2))], alignment: .center){
